6b03084b514dfec08f7e05097cae858700edd0bd
charlatan <yllyou333@163.com>
2022-01-11 16:27:07 +0800

    22/1/11 http的Content-type 【下午】

---
+++ com/charlatan/self_study/Node/基础/http/Content-Type.js
@@ -0,0 +1,39 @@
+/**
+ * Date:2022/1/11 14:12 21
+ * Name:Content-Type.js
+ * Path:Web代码/src/com/charlatan/self_study/Node/基础/http
+ * ProjectName:WWW
+ * Author:charlatan
+ *
+ *  Il n'ya qu'un héroïsme au monde : c'est de voir le monde tel qu'il est et de l'aimer.
+ */
+
+
+let http = require('http')
+let server = http.createServer()
+server.on('request', function (request, response) {
+	/*
+	* 服务器向浏览器发送的数据是 utf8 编码形式的，浏览器解析时会按照计算机本地的语言环境进行解析
+	* 因为编码不统一造成乱码
+	*
+	* 解决方式，告诉浏览器的解码形式
+	*   Content-Type 用来指定发送来的编码类型 将默认的 GBK 改成 utf-8
+	*   text/html   ：   HTML格式
+		text/plain  ：   纯文本格式
+		text/xml    ：   XML格式
+		image/gif   ：   gif图片格式
+		image/jpeg  ：   jpg图片格式
+		image/png   ：   png图片格式
+	* */
+	
+	if (request.url === '/plain') {
+		response.setHeader('Content-Type', "text/plain ; charset=utf-8")
+		response.end("我是普通文本1")
+	} else if (request.url === '/html') {
+		response.setHeader('Content-type', 'text/html;charset=utf-8')
+		response.end('<p style="color: #f50b0b;">我是设置为html文本输出的内容</p>')
+	}
+})
+server.listen('8300', function () {
+	console.log("8300端口启动");
+})
---
+++ com/charlatan/self_study/Node/基础/http/Content-type-2.js
@@ -0,0 +1,73 @@
+/**
+ * Date:2022/1/11 15:29 47
+ * Name:Content-type-2.js
+ * Path:Web代码/src/com/charlatan/self_study/Node/基础/http
+ * ProjectName:WWW
+ * Author:charlatan
+ *
+ *  Il n'ya qu'un héroïsme au monde : c'est de voir le monde tel qu'il est et de l'aimer.
+ */
+
+// 使用 Content-type 处理更多数据
+let http = require('http')
+let fs = require('fs')
+let server = http.createServer()
+server.on("request", function (request, response) {
+	if (request.url === '/html') {
+		response.setHeader('Content-type', 'text/html ;charset=utf-8')
+		fs.readFile('./test-file/Content-type-test.html', function (err, data) {
+			if (!err) {
+				response.end(data.toString())
+			} else {
+				response.end(`	<p style="
+												position: absolute;
+												left:  0;
+												right: 0;
+												text-align: center;
+												font:700 80px '';
+												user-select: none">
+											404 Not Found
+										</p>`)
+			}
+		})
+	} else if (request.url === '/img') {
+		response.setHeader('Content-type', 'image/jpeg')
+		fs.readFile('./test-file/img.jpg', function (err, data) {
+			if (!err) {
+				response.end(data)
+			} else {
+				response.end(`	<p style="
+												position: absolute;
+												left:  0;
+												right: 0;
+												text-align: center;
+												font:700 80px '';
+												user-select: none">
+											404 Not Found
+										</p>`)
+			}
+		})
+	} else if (request.url === '/gif') {
+		response.setHeader('Content-type', 'image/gif')
+		fs.readFile('./test-file/gif.gif', function (err, data) {
+			if (!err) {
+				response.end(data)
+			} else {
+				response.end(`	<p style="
+												position: absolute;
+												left:  0;
+												right: 0;
+												text-align: center;
+												font:700 80px '';
+												user-select: none">
+											404 Not Found
+										</p>`)
+			}
+		})
+	}
+	
+})
+
+server.listen('3000', function () {
+	console.log("3000端口监听中...");
+})
