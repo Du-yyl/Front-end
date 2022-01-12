2f724b61a912eed79de157bf321e81e3739e18a8
charlatan <yllyou333@163.com>
2022-01-12 20:03:10 +0800

    22/1/12 整理了node的笔记 【上午】

--- com/charlatan/self_study/Node/Express/express-hello-word.js
+++ com/charlatan/self_study/Node/Express/express-hello-word.js
@@ -14,20 +14,6 @@ let express = require ('express');
 // 创建服务器
 let server = express ();
 
-// 公开指定文件夹
-// 公开后可以直接通过 ./文件夹名称/ xxx 的形式进行访问
-// 语法：
-//   server.user('/文件夹名称/',express.static('./文件夹名称/'))
-// server.use ('/file-test/', express.static ('./file-test/'));
-
-// 当第一个参数省略的时候，对应的访问路径也要进行省略，访问的时候直接：
-// /指定文件就行
-// server.use(express.static('./file-test'))
-
-// 另一种特殊形式
-// 这里的第一个参数并不知指定文件夹的名称，而是别名，访问的时候必须使用 /file/ 进行访问，否则无效
-server.use ('/file/', express.static ('./file-test/'));
-
 // 收到的请求形式和 url
 server.get ('/', function (request, response) {
 	console.log (request.url);
---
+++ com/charlatan/self_study/Node/模板引擎/node中的模板引擎.js
@@ -0,0 +1,11 @@
+/**
+ * Date:2022/1/12 18:57 16
+ * Name:node中的模板引擎.js
+ * Path:Web代码/src/com/charlatan/self_study/Node/Express/模板引擎
+ * ProjectName:WWW
+ * Author:charlatan
+ *
+ *  Il n'ya qu'un héroïsme au monde : c'est de voir le monde tel qu'il est et de l'aimer.
+ */
+
+// art-template 模板引擎
