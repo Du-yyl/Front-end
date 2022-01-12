4e5a03da9ee891c137db04f092b3b677f06b8c86
charlatan <yllyou333@163.com>
2022-01-12 17:10:16 +0800

    22/1/12 整理了node的笔记 【上午】

--- com/charlatan/self_study/Node/Express/express-hello-word.js
+++ com/charlatan/self_study/Node/Express/express-hello-word.js
@@ -18,16 +18,23 @@ let server = express ();
 // 公开后可以直接通过 ./文件夹名称/ xxx 的形式进行访问
 // 语法：
 //   server.user('/文件夹名称/',express.static('./文件夹名称/'))
-server.use ('/file-test/', express.static ('./file-test/'));
+// server.use ('/file-test/', express.static ('./file-test/'));
+
+// 当第一个参数省略的时候，对应的访问路径也要进行省略，访问的时候直接：
+// /指定文件就行
+// server.use(express.static('./file-test'))
+
+// 另一种特殊形式
+// 这里的第一个参数并不知指定文件夹的名称，而是别名，访问的时候必须使用 /file/ 进行访问，否则无效
+server.use ('/file/', express.static ('./file-test/'));
 
 // 收到的请求形式和 url
 server.get ('/', function (request, response) {
-  console.log (request.url);
-  response.send ('hello world');
+	console.log (request.url);
+	response.send ('hello world');
 });
 
 // 开启监听
 server.listen ('3000', function () {
-  console.log ('3000 is listening ... ');
+	console.log ('3000 is listening ... ');
 });
-
