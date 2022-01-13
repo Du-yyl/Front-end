ab2f2d4a90a11f264608ee4dcecf3036eac993c2
charlatan <yllyou333@163.com>
2022-01-13 15:14:43 +0800

    22/1/3 模板引擎拿数据 【中午】

--- com/charlatan/self_study/Node/模板引擎/node中的模板引擎.js
+++ com/charlatan/self_study/Node/模板引擎/node中的模板引擎.js
@@ -9,26 +9,26 @@
  */
 
 // art-template 模板引擎
-let template = require ('art-template');
-let fs = require ('fs');
+let template = require('art-template')
+let fs = require('fs')
 
-fs.readFile ('node中的模板引擎-test.html', function (err, data) {
-	
-	if (!err) {
-		
-		/*
-		* 这里使用读取到的 html 文件数据来进行数据内容的交换，因为读取文件的方法是异步的，所以要在方法内进行数据的使用，方法外访问不到
-		*  */
-		console.log (template.render (data.toString (), tools));
-	} else {
-		console.log (err);
-	}
-});
+fs.readFile('node中的模板引擎-test.html', function (err, data) {
+  
+  if (!err) {
+    
+    /*
+    * 这里使用读取到的 html 文件数据来进行数据内容的交换，因为读取文件的方法是异步的，所以要在方法内进行数据的使用，方法外访问不到
+    *  */
+    console.log(template.render(data.toString(), tools))
+  } else {
+    console.log(err)
+  }
+})
 
 let tools = {
-	name: 'Tom', data1: '数据1', data2: '数据2', data3: '数据3', data: ['我是', '多个', '数据', '的', '展示'],
-};
+  name: 'Tom', data1: '数据1', data2: '数据2', data3: '数据3', data: ['我是', '多个', '数据', '的', '展示'],
+}
 
-console.log (template.render ('value:{{ value }}', {
-	value: 123,
-}));
+console.log(template.render('value:{{ value }}', {
+  value: 123,
+}))
