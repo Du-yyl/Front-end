083c7da0485a76c08feb95a5cbc3ae73410c2c33
charlatan <yllyou333@163.com>
2022-01-16 11:39:36 +0800

    22/1/16 做响应式主页 【中午】

---
+++ com/charlatan/practice/LessTest/响应式的Microsoft主页/Microsoft.js
@@ -0,0 +1,10 @@
+/**
+ * Time:2022/1/15 21:32 54
+ * Name:Microsoft.js
+ * Path:Web代码/src/com/charlatan/practice/LessTest/响应式的Microsoft主页
+ * ProjectName:WWW
+ * Author:charlatan
+ *
+ *  Il n'ya qu'un héroïsme au monde : c'est de voir le monde tel qu'il est et de l'aimer.
+ */
+
