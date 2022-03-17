6c0f2ee266bb729eed6a5b3e2e1a657ed8ed3462
charlatan <yllyou333@163.com>
2022-03-17 16:04:52 +0800

    22-3-16 完成Vue

---
+++ com/charlatan/self_study/Vue/Vue 3.x/Vue3_test/vue3/__mains__/18、teleport/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="div">
+    祖组件
+    <Child></Child>
+  </div>
+</template>
+
+<script>
+
+import { customRef, watch, toRefs } from '@vue/runtime-dom'
+import Child from '@/components/Child'
+import Dialog from '@/components/Dialog'
+import { provide, reactive } from 'vue'
+
+const { ref } = require('@vue/runtime-dom')
+
+export default {
+  name: 'App',
+  components: {
+    Child,
+  },
+}
+</script>
+
+<style>
+.div {
+  background: yellow;
+  border: 10px solid bisque;
+}
+</style>
---
+++ com/charlatan/self_study/Vue/Vue 3.x/Vue3_test/vue3/__mains__/18、teleport/src/components/Son.vue
@@ -0,0 +1,36 @@
+<!--
+  ~ Time:2022/3/17 11:41 55
+  ~ Name:Son.vue
+  ~ Path:Web/src/com/charlatan/self_study/Vue/Vue 3.x/Vue3_test/vue3/src/components
+  ~ ProjectName:WWW
+  ~ Author:charlatan
+  ~
+  ~  Il n'ya qu'un héroïsme au monde : c'est de voir le monde tel qu'il est et de l'aimer.
+  -->
+
+<template>
+  <div id="Son">
+    孙组件
+    <br>
+    <Dialog></Dialog>
+  </div>
+</template>
+
+<script>
+const { inject } = require('@vue/runtime-dom')
+import Dialog from './Dialog'
+
+export default {
+  name: 'Son',
+  components: {
+    Dialog,
+  },
+}
+</script>
+
+<style>
+#Son {
+  background: red;
+  border: 10px solid bisque;
+}
+</style>
