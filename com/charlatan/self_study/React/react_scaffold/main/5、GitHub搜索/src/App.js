bbeb96d11989b37ff4e8d85b207241d34dccca18
charlatan <yllyou333@163.com>
2022-03-26 00:25:49 +0800

    22-03-24 GITHUB

---
+++ com/charlatan/self_study/React/react_scaffold/main/5、GitHub搜索/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import './App.css'
+import Search from './components/Search'
+import List from './components/List'
+
+class App extends Component {
+  state = {
+    showList: [],
+    reason: '',
+    going: '',
+    errMsg:'网络错误'
+  }
+  
+  render () {
+    let { showList, reason, going ,errMsg} = this.state
+    return (
+      <div id="app">
+        <Search
+          id="Search"
+          reason={reason}
+          going={going}
+          updateList={this.updateList}/>
+        
+        <List
+          id="List"
+          showList={showList}
+          reason={reason}
+          errMsg={errMsg}
+          going={going}/>
+      </div>
+    )
+  }
+  
+  updateList = (showList, going, reason,errMsg='网络错误') => {
+    this.setState({
+      showList,
+      going,
+      reason,
+      errMsg
+    })
+  }
+}
+
+export default App
