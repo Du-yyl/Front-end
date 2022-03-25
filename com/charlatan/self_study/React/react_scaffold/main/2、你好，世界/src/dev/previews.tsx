c0d6a24c0517bfd97c7d20e7f3327a0477a8a5b8
charlatan <yllyou333@163.com>
2022-03-25 18:00:01 +0800

    22-03-24 TOLIST

--- com/charlatan/self_study/React/react_scaffold/main/2、你好，世界/src/dev/previews.tsx
+++ com/charlatan/self_study/React/react_scaffold/main/2、你好，世界/src/dev/previews.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox'
 import { PaletteTree } from './palette'
 import Welcome from '../components/Welcome'
+import App from '../../../../src/App'
+import Add from '../../../../src/components/Add'
+import Lists from '../../../../src/components/Lists'
 
 const ComponentPreviews = () => {
   return (
     <Previews palette={<PaletteTree/>}>
       <ComponentPreview path="/Welcome">
         <Welcome/>
+      </ComponentPreview>
+      <ComponentPreview path="/App">
+        <App/>
+      </ComponentPreview>
+      <ComponentPreview path="/Add">
+        <Add/>
+      </ComponentPreview>
+      <ComponentPreview
+        path="/Lists">
+        <Lists/>
       </ComponentPreview>
     </Previews>
   )
--- com/charlatan/self_study/React/react_scaffold/src/index.js
+++ com/charlatan/self_study/React/react_scaffold/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { DevSupport } from '@react-buddy/ide-toolbox'
-import { ComponentPreviews, useInitial } from './dev'
 
-ReactDOM.render(<DevSupport
-  ComponentPreviews={ComponentPreviews}
-  useInitialHook={useInitial}
->
-  <App/>
-</DevSupport>, document.getElementById('root'))
+ReactDOM.render(<App/>  , document.getElementById('root'))
 
